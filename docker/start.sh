316b20c50ea4666197e1929a2ee24b7285092291
Ibrahim <ibrahimhammashsofteng@gmail.com>
2026-04-25 01:19:56 +0300

    fix assets on deployment

--- docker/start.sh
+++ docker/start.sh
@@ -10,6 +10,9 @@ php artisan config:clear
 
 php artisan migrate --force
 php artisan db:seed --force
+php artisan vendor:publish --tag=laravel-assets --force
+php artisan filament:assets
+php artisan vendor:publish --tag=scribe-assets --force
 php artisan config:cache
 
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
