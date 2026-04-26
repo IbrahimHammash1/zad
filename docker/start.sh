a17ebe4964bf95ec54544b4b538fb80b0bb726ad
Ibrahim <ibrahimhammashsofteng@gmail.com>
2026-04-26 18:07:43 +0300

    updates

--- docker/start.sh
+++ docker/start.sh
@@ -13,6 +13,7 @@ php artisan db:seed --force
 php artisan vendor:publish --tag=laravel-assets --force
 php artisan filament:assets
 php artisan vendor:publish --tag=scribe-assets --force
+php artisan scribe:generate --force
 php artisan config:cache
 
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
